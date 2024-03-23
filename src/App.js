ceca4b47b38d4a08f28cb975066cf5c631d6dfb3
JJ Rpo <juanjogithub@gmail.com>
2024-03-23 14:33:34 -0500

    contador de clicks con react

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logoNombre from './imagenes/logoNombre.png';
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+  return (
+    <div className='App'>
+      <div className='nombre-logo-contenedor'>
+        <img 
+          className='nombre-logo'
+          src={logoNombre}
+          alt='logo con nombre' />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
